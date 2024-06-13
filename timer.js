ac0c21cad3538229fc9d1519a3096c6199dc9437
Klay <55288045+kdalpra@users.noreply.github.com>
2024-06-12 23:18:00 -0400

    locked

--- timer.js
+++ timer.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jul 1, 2023 18:0:0").getTime();
+var countDownDate = new Date("Jun 20, 2024 18:0:0").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
